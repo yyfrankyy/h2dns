35d2de161ac8273762bb850a5386bfaafa7e61e2
yyfrankyy <yyfrankyy@gmail.com>
2016-10-12 14:56:58 +0800

    bugfix: fix wrong response due to AAAA query when 'edns_client_subnet' is empty (#8)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dnsd = require('./dnsd/named');
 const spdy = require('spdy');
 const randomstring = require("randomstring");
 const forwardUrl = 'https://dns.google.com:443/resolve';
-const url = require('url'); 
+const url = require('url');
 const resolver = url.parse(forwardUrl);
 
 const request = require('request').defaults({
@@ -26,36 +26,39 @@ const server = dnsd.createServer((req, res) => {
   let question = req.question[0], hostname = question.name;
   let timeStamp = `[${req.id}/${req.connection.type}] ${req.opcode} ${hostname} ${question.class} ${question.type}`;
   console.time(timeStamp);
-  
+
   // TODO unsupported due to dnsd's broken implementation.
   if (SupportTypes.indexOf(question.type) === -1) {
     console.timeEnd(timeStamp);
     return res.end();
   }
-  
+
   // API clients concerned about possible side-channel privacy attacks
   // using the packet sizes of HTTPS GET requests can use this to make all
-  // requests exactly the same size by padding requests with random data. 
+  // requests exactly the same size by padding requests with random data.
   let padding = randomstring.generate({
-    // maximum dnslength+NSEC3PARAM.length (longest possible Type now) 
+    // maximum dnslength+NSEC3PARAM.length (longest possible Type now)
     // minus current To make always equal query lenght url
     length: 263 - question.name.length - question.type.length,
-    // safe but can be more extended chars-_ 
+    // safe but can be more extended chars-_
     charset: 'alphanumeric'
   });
-  
+
   let query = {
-    edns_client_subnet: subnet || '',
     name: hostname,
     type: Constants.type_to_number(question.type),
-    random_padding:   padding
-  }   
+    random_padding: padding
+  }
+
+  if (subnet) {
+    query.edns_client_subnet = subnet;
+  }
 
   request({
     url: forwardUrl,
     qs: query
-  }, (err, response, output) => {    
-    if (output && output.Answer) {      
+  }, (err, response, output) => {
+    if (output && output.Answer) {
       res.answer = output.Answer.map(rec => {
         rec.ttl = rec.TTL;
         rec.type = Constants.type_to_label(rec.type);
